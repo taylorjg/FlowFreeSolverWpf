29be9db882bbefadb6c9a171e321c39c07deb106
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-30 23:43:38 +0100

    Made a start on MVVM

--- FlowFreeSolverWpf/ViewModel/MainWindowViewModel.cs
+++ FlowFreeSolverWpf/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,105 @@
-﻿using System.ComponentModel;
+﻿using System.Windows.Input;
+using FlowFreeSolverWpf.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace FlowFreeSolverWpf.ViewModel
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ViewModelBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        private GridDescription _selectedGrid;
+        private DotColour _selectedDotColour;
+        private string _statusMessage;
+        private ICommand _solveCommand;
+        private ICommand _clearCommand;
+        private ICommand _selectedGridChangedCommand;
 
-        private void RaisePropertyChanged(string propertyName)
+        public MainWindowViewModel()
         {
-            var handler = PropertyChanged;
+            GridDescriptions = Grids.GridDescriptions;
+            DotColours = Model.DotColours.Colours;
+            StatusMessage = string.Empty;
+        }
 
-            if (handler != null)
+        public GridDescription[] GridDescriptions { get; private set; }
+        public DotColour[] DotColours { get; private set; }
+
+        public GridDescription SelectedGrid
+        {
+            get
             {
-                handler(this, new PropertyChangedEventArgs(propertyName));
+                return _selectedGrid;
             }
+            set
+            {
+                _selectedGrid = value;
+                RaisePropertyChanged("SelectedGrid");
+            }
+        }
+
+        public DotColour SelectedDotColour
+        {
+            get
+            {
+                return _selectedDotColour;
+            }
+            set
+            {
+                _selectedDotColour = value;
+                RaisePropertyChanged("SelectedDotColour");
+            }
+        }
+
+        public string StatusMessage
+        {
+            get
+            {
+                return _statusMessage;
+            }
+            private set
+            {
+                _statusMessage = value;
+                RaisePropertyChanged("StatusMessage");
+            }
+        }
+
+        public ICommand SolveCommand
+        {
+            get { return _solveCommand ?? (_solveCommand = new RelayCommand(OnSolve, OnCanSolve)); }
+        }
+
+        public ICommand ClearCommand
+        {
+            get { return _clearCommand ?? (_clearCommand = new RelayCommand(OnClear, OnCanClear)); }
+        }
+
+        // http://stackoverflow.com/questions/3131142/mvvm-light-silverlight-using-eventtocommand-with-a-combobox
+
+        public ICommand SelectedGridChangedCommand
+        {
+            get { return _selectedGridChangedCommand ?? (_selectedGridChangedCommand = new RelayCommand(OnSelectedGridChanged)); }
+        }
+
+        private void OnSolve()
+        {
+        }
+
+        private bool OnCanSolve()
+        {
+            return true;
+        }
+
+        private void OnClear()
+        {
+        }
+
+        private bool OnCanClear()
+        {
+            return true;
+        }
+
+        private void OnSelectedGridChanged()
+        {
         }
     }
 }
