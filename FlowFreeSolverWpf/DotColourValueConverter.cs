ed3d2d84b8489695029616e8b466cfdb01368c78
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-30 19:40:29 +0100

    Fixed the combobox colourName binding problem
    Added a value converter

---
+++ FlowFreeSolverWpf/DotColourValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using FlowFreeSolverWpf.Model;
+
+namespace FlowFreeSolverWpf
+{
+    public class DotColourValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dotColour = (DotColour) value;
+            return dotColour.ToWpfColour();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
