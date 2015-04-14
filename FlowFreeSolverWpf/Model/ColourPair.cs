775175019dd51b045e788498e4316e3e372c1bd9
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-14 09:37:51 +0100

    ColourPair is also now properly immutable

--- FlowFreeSolverWpf/Model/ColourPair.cs
+++ FlowFreeSolverWpf/Model/ColourPair.cs
@@ -2,15 +2,30 @@
 {
     public class ColourPair
     {
+        private readonly Coords _startCoords;
+        private readonly Coords _endCoords;
+        private readonly DotColour _dotColour;
+
         public ColourPair(Coords startCoords, Coords endCoords, DotColour dotColour)
         {
-            StartCoords = startCoords;
-            EndCoords = endCoords;
-            DotColour = dotColour;
+            _startCoords = startCoords;
+            _endCoords = endCoords;
+            _dotColour = dotColour;
         }
 
-        public Coords StartCoords { get; private set; }
-        public Coords EndCoords { get; private set; }
-        public DotColour DotColour { get; set; }
+        public Coords StartCoords
+        {
+            get { return _startCoords; }
+        }
+
+        public Coords EndCoords
+        {
+            get { return _endCoords; }
+        }
+
+        public DotColour DotColour
+        {
+            get { return _dotColour; }
+        }
     }
 }
